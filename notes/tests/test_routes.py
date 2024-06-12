4a5eaf185bfb671f34bed31231bb6dc60ffd692b
Albina Shaibakova <albinashaibakova@gmail.com>
2024-06-12 15:40:51 +0500

    added pytest/test_routes

--- notes/tests/test_routes.py
+++ notes/tests/test_routes.py
@@ -4,8 +4,6 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
-from unittest import skip
-
 from notes.models import Note
 
 User = get_user_model()
@@ -37,6 +35,17 @@ class TestRoutes(TestCase):
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
 
+    def test_pages_availability_for_auth_user(self):
+        urls = (
+            ('notes:list', None),
+            ('notes:add', None),
+            ('notes:success', None),
+        )
+        for name in urls:
+            with self.subTest(name=name):
+                url = reverse(name)
+                response = self.client.get(url)
+                assert response.status_code == HTTPStatus.OK
 
 
     def test_availability_for_different_users(self):
@@ -52,10 +61,10 @@ class TestRoutes(TestCase):
                     response = self.client.get(url)
                     self.assertEqual(response.status_code, status)
 
-    def test_redirect_for_anonymous_client(self):
-        self.client.force_login(self.author)
+    def test_redirect(self):
         login_url = reverse('users:login')
-        for name in ('notes:edit', 'notes:delete'):
+        for name in ('notes:edit', 'notes:delete', 'notes:detail',
+                     'notes:add', 'notes:success', 'notes:list'):
             with self.subTest(name=name):
                 url = reverse(name, args=(self.note.slug,))
                 redirect_url = f'{login_url}?next={url}'
