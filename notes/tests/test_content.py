903f4e650c29bfeed7573b79db2e051db6cc6851
Albina Shaibakova <albinashaibakova@gmail.com>
2024-06-12 18:20:13 +0500

    added test/test_routes

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -1,36 +1,50 @@
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
 from notes.models import Note
+from notes.forms import NoteForm
 
 
 User = get_user_model()
 
 
-class TestHomePage(TestCase):
+class TestNoteListPage(TestCase):
 
     HOME_URL = reverse('notes:list')
-
+    NOTE_ADD_URL = reverse('notes:add')
 
     @classmethod
     def setUpTestData(cls):
-        all_notes = []
         cls.author = User.objects.create(username='Автор заметки')
-        for index in range(settings.NOTES_COUNT_ON_HOME_PAGE + 1):
-            
-            notes = Note(title=f'Заметка {index}',
-                         text='Текст заметки',
-                         slug=f'slug{index}',
-                         author=cls.author)
-            all_notes.append(notes)
-        Note.objects.bulk_create(all_notes)
-        print(Note.objects.all())
+        cls.not_author = User.objects.create(username='Не автор')
+        cls.note = Note.objects.create(title='Заметка',
+                    text='Текст заметки',
+                    slug='slug',
+                    author=cls.author)
 
-    def test_new_count(self):
+
+    def test_note_in_list_for_author(self):
         self.client.force_login(self.author)
         response = self.client.get(self.HOME_URL)
         object_list = response.context['object_list']
-        notes_count = object_list.count()
-        self.assertEqual(notes_count, settings.NOTES_COUNT_ON_HOME_PAGE)
+        self.assertIn(self.note, object_list)
+
+    def test_note_in_list_for_not_author(self):
+        self.client.force_login(self.not_author)
+        response = self.client.get(self.HOME_URL)
+        object_list = response.context['object_list']
+        self.assertNotIn(self.note, object_list)
+
+    def test_create_note_page_contains_form(self):
+        self.client.force_login(self.author)
+        response = self.client.get(self.NOTE_ADD_URL)
+        self.assertIn('form', response.context)
+        self.assertIsInstance(response.context['form'], NoteForm)
+
+    def test_edit_note_page_contains_form(self):
+        self.client.force_login(self.author)
+        url = reverse('notes:edit', args=(self.note.slug,))
+        response = self.client.get(url)
+        self.assertIn('form', response.context)
+        self.assertIsInstance(response.context['form'], NoteForm)
